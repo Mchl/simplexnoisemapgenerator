833277066bdfc285765bef784edb538c73152413
Michal Jarosz <michal.jarosz@stepstone.com>
2016-06-15 20:11:40 +0200

    Fix request logger

--- loggers.js
+++ loggers.js
@@ -5,17 +5,28 @@ const mainLogger = bunyan.createLogger({name: 'MAPGEN'})
 
 const requestLogger = mainLogger.child({component: 'REQUEST'})
 
-const logRequestMiddleware = (res, req, next) => {
+const logRequestMiddleware = (req, res, next) => {
   const startTime = process.hrtime()
   
   req.uuid = uuid()
   
-  requestLogger.info(req.uuid.substr(0,8), {method: req.method, path: req.path})
-  console.log(req)
+  requestLogger.info(req.uuid.substr(0, 8), req.method, req.originalUrl)
   res.on('finish', function responseSent() {
-      const diff = process.hrtime(time);
-      req.log.info(req.uuid.substr(0,8), 'response sent', {uuid: req.uuid, duration: diff[0] * 1e3 + diff[1] * 1e-6});
-    });
+    const diff = process.hrtime(startTime);
+    const responseData = {
+      duration: diff[0] * 1e3 + diff[1] * 1e-6,
+      statusCode: res.statusCode,
+      statusMessage: res.statusMessage
+    }
+
+    if (res.statusCode < 400) {
+      requestLogger.info(req.uuid.substr(0, 8), 'response sent', responseData)
+    } else if (res.statusCode < 500) {
+      requestLogger.warn(req.uuid.substr(0, 8), 'response sent', responseData)
+    } else {
+      requestLogger.error(req.uuid.substr(0, 8), 'response sent', responseData)
+    }
+  })
   next()
 }
 
