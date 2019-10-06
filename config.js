7db79ca9890914fb4db50eb0c20800a133bd6cfe
Michal Jarosz <michal.jarosz@stepstone.com>
2019-10-06 13:12:06 +0200

    Use bezier curves to alter the landmass profile
    Less mountains, more lowlands

--- config.js
+++ config.js
@@ -1,11 +1,13 @@
 const maxHeight = 10000
-const seaLevel = 5500
+const seaLevel = 5000
 const landHeight = maxHeight - seaLevel
 const elevationZones = [250, 1000, 2500, Number.MAX_SAFE_INTEGER]
+const landMassProfileEnabled = true
 
 module.exports = {
   maxHeight,
   seaLevel,
   landHeight,
-  elevationZones
+  elevationZones,
+  landMassProfileEnabled
 }
